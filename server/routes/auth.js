459d3ca9e82cd6b798d2891b4337e44cf627c4e5
jfunkeye <jfunkeye@gmail.com>
2025-11-30 12:18:07 +0100

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/authController');
-const auth = require('../middleware/auth'); 
 
+// Test route
+router.get('/', (req, res) => {
+  res.json({ message: 'Auth routes are working!' });
+});
 
+// Auth endpoints
 router.post('/signup', ctrl.signup);
 router.post('/verify', ctrl.verify);
 router.post('/login', ctrl.login);
 router.post('/forgot', ctrl.forgot);
 router.post('/reset', ctrl.reset);
-
-
-
 
 module.exports = router;
